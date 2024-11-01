2c73e3ab8e5facbf5d1dcd545d53d0c74e64ac8d
diro <diro.fan@gmail.com>
2024-11-01 20:30:54 +0800

    load variables from env: DB_USER, DB_PASSWORD, DB_NAME

--- main.go
+++ main.go
@@ -5,19 +5,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	_ "github.com/lib/pq" // 改用 PostgreSQL 驱动
 )
 
 const (
-	dbHost     = "lifeplan-instance-1.c1w4eg0mwku5.us-west-2.rds.amazonaws.com"
-	dbPort     = "5432"
-	dbUser     = "username"
-	dbPassword = "password"
-	dbName     = "dbname"
+	dbHost = "lifeplan-instance-1.c1w4eg0mwku5.us-west-2.rds.amazonaws.com"
+	dbPort = "5432"
 )
 
 func main() {
+	// 从环境变量获取数据库凭据
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// 检查必要的环境变量是否存在
+	if dbUser == "" || dbPassword == "" || dbName == "" {
+		log.Fatal("必要的数据库环境变量未设置 (DB_USER, DB_PASSWORD, DB_NAME)")
+	}
+
 	// 構建連接字符串
 	dbURI := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=require",
 		dbUser, dbPassword, dbHost, dbPort, dbName)
